304110687097ede608c8d8518588377158f28414
Rhunal <rhunalsawant@gmail.com>
2025-01-13 20:00:23 +0530

    when the input is an empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,5 @@
 class StringCalculator
   def add(numbers)
+    numbers.split(',').sum(&:to_i)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,12 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
+    let(:calculator) { StringCalculator.new }
+
+    context 'when the input is an empty string' do
+      it 'should return 0' do
+        expect(calculator.add('')).to eq(0)
+      end
+    end
   end
 end
