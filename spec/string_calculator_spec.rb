df899a725c48e9b2a802f19ff602cc07c5d7e52b
Rhunal <rhunalsawant@gmail.com>
2025-01-14 02:40:09 +0530

    refactored string_calculator_spec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,81 +5,101 @@ RSpec.describe StringCalculator do
     let(:calculator) { StringCalculator.new }
 
     context 'when the input is an empty string' do
-      it 'should return 0' do
+      it 'returns 0' do
         expect(calculator.add('')).to eq(0)
       end
     end
 
     context 'when the input is a single number string' do
-      it 'should return number itself' do
-        expect(calculator.add('1')).to eq(1)
-        expect(calculator.add('8')).to eq(8)
+      it 'returns the number itself' do
+        [1, 8].each do |num|
+          expect(calculator.add(num.to_s)).to eq(num)
+        end
       end
     end
 
     context 'when the input is a two comma-separated numbers string' do
-      it 'should return the sum of numbers' do
-        expect(calculator.add('1, 2')).to eq(3)
-        expect(calculator.add('8, 5')).to eq(13)
+      it 'returns the sum of the numbers' do
+        {
+          '1, 2' => 3,
+          '8, 5' => 13
+        }.each do |input, expected_sum|
+          expect(calculator.add(input)).to eq(expected_sum)
+        end
       end
     end
 
-    context 'when the input is a multiple comma-separated numbers string' do
-      it 'should return the sum of numbers' do
-        expect(calculator.add('1, 2, 3')).to eq(6)
-        expect(calculator.add('1,2, 3,4')).to eq(10)
-        expect(calculator.add('8, 5, 1, 2, 3, 5, 4, 7')).to eq(35)
+    context 'when the input contains multiple comma-separated numbers' do
+      it 'returns the sum of the numbers' do
+        {
+          '1, 2, 3' => 6,
+          '1,2, 3,4' => 10,
+          '8, 5, 1, 2, 3, 5, 4, 7' => 35
+        }.each do |input, expected_sum|
+          expect(calculator.add(input)).to eq(expected_sum)
+        end
       end
     end
 
-    context 'when the input is having new lines between numbers string' do
-      it 'should return the sum of numbers' do
-        expect(calculator.add('1\n2, 3')).to eq(6)
-        expect(calculator.add('8\n5 \n 2')).to eq(15)
-        expect(calculator.add('8, 5\n1, 2, 3\n 5, 4 \n7')).to eq(35)
+    context 'when the input contains new lines between numbers' do
+      it 'returns the sum of the numbers' do
+        {
+          '1\n2, 3' => 6,
+          '8\n5 \n 2' => 15,
+          '8, 5\n1, 2, 3\n 5, 4 \n7' => 35
+        }.each do |input, expected_sum|
+          expect(calculator.add(input)).to eq(expected_sum)
+        end
       end
     end
 
-    context 'when the input is having different delimiters betwen numbers string' do
-      it 'should return the sum of numbers' do
-        expect(calculator.add('//;\n1;2')).to eq(3)
-        expect(calculator.add("//;\n1;2,3\n4")).to eq(10)
+    context 'when the input contains custom delimiters' do
+      it 'returns the sum of the numbers' do
+        {
+          '//;\n1;2' => 3,
+          "//;\n1;2,3\n4" => 10
+        }.each do |input, expected_sum|
+          expect(calculator.add(input)).to eq(expected_sum)
+        end
       end
     end
 
     context 'when the input contains negative numbers' do
-      it "should throw an exception as 'negative numbers not allowed <negative_number>'" do
-        expect do
-          calculator.add('1,-2,3')
-        end.to raise_error('negative numbers not allowed: -2')
-
-        expect do
-          calculator.add('1,-2, -3,-7,-8, -9')
-        end.to raise_error('negative numbers not allowed: -2, -3, -7, -8, -9')
+      it "raises an exception with 'negative numbers not allowed <negative_number>'" do
+        {
+          '1,-2,3' => 'negative numbers not allowed: -2',
+          '1,-2, -3,-7,-8, -9' => 'negative numbers not allowed: -2, -3, -7, -8, -9'
+        }.each do |input, error_message|
+          expect { calculator.add(input) }.to raise_error(error_message)
+        end
       end
     end
 
-    context 'when the input contains numbers bigger than 1000' do
-      it 'should ignore numbers bigger than 1000 and return the sum of valid numbers' do
-        expect(calculator.add('1,1000')).to eq(1001)
-        expect(calculator.add('1,1001')).to eq(1)
+    context 'when the input contains numbers greater than 1000' do
+      it 'ignores numbers greater than 1000 and returns the sum of valid numbers' do
+        {
+          '1,1000' => 1001,
+          '1,1001' => 1
+        }.each do |input, expected_sum|
+          expect(calculator.add(input)).to eq(expected_sum)
+        end
       end
     end
 
     context 'when the input contains custom delimiters of any length' do
-      it 'should return the sum of numbers' do
+      it 'returns the sum of the numbers' do
         expect(calculator.add("//[***]\n1***2***3")).to eq(6)
       end
     end
 
-    context "when the input contains multiple delimiters like '//[delim1][delim2]\n'" do
-      it 'should return the sum of numbers' do
+    context 'when the input contains multiple delimiters' do
+      it 'returns the sum of the numbers' do
         expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
       end
     end
 
-    context "when the input contains multiple delimiters of any length" do
-      it 'should return the sum of numbers' do
+    context 'when the input contains multiple delimiters of any length' do
+      it 'returns the sum of the numbers' do
         expect(calculator.add("//[***][%%]\n1***2%%3")).to eq(6)
       end
     end
